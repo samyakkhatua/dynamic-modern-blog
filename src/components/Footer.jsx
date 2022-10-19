d85aa2ecd9e30210d49f2b277891cb6bdc8027c1
samyakkhatua <samyakkhatua@duck.com>
2022-10-19 19:42:24 +0530

    Footer style updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className=" bg-[#ccf0fa] h-[150px] mt-32">
-        <div className="flex columns-4 gap-[50px] w-[70%] mx-auto pt-[50px]">
+        <div className="flex columns-4 gap-[50px] w-[80%] mx-auto pt-[50px] justify-between ">
           {/* logo & brand name  */}
           <div className=" ">
             <a href="" class="flex items-center ">
@@ -13,26 +13,43 @@ const Footer = () => {
                 class="mr-3 h-6 sm:h-9"
                 alt="Samyak Img"
               />
-              <span class="self-center text-3xl font-semibold whitespace-nowrap dark:text-white ">
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ">
                 Samyak Khatua
               </span>
+              <p className="ml-4 font-thin text-4xl text-[#94999b]">|</p>
             </a>{" "}
           </div>
 
           {/* email id */}
-          <div>
+          <div className="text-[12px] font-bold underline my-auto">
             <p>hello@samyakkhatua.in</p>
           </div>
 
           {/* T&C */}
+          <div className="items-center">
+            <p className="text-[#94999b] text-[12px] text-center mb-2">
+              © 2022 company. All right reserved.
+            </p>
+            <div className="flex justify-between gap-3">
+              <p className="text-[12px] font-bold underline">
+                Terms & Conditions
+              </p>
+              <p className="text-[14px] text-[#94999b]">|</p>
+              <p className="text-[12px] font-bold underline">Privacy Policy</p>
+            </div>
+            
+          </div>
+
           <div>
-            <p>2022 company. All right reserved.</p>
-            <p>Terms & Conditions | Privacy Policy</p>
+          <p className="ml-4 font-thin text-4xl text-[#94999b]">|</p>
+
           </div>
 
           {/* social icons */}
           <div>
-            <p>Follow me:</p>
+
+            <p className="text-[#94999b] text-[12px] bold mb-2">
+              Follow me:</p>
             <div className="flex gap-4">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
