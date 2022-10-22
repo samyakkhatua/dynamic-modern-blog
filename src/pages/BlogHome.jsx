2e01828fcd6c5d06ae3db303fd1fdb2df13b2928
samyakkhatua <samyakkhatua@duck.com>
2022-10-22 23:44:02 +0530

    Comments added

--- src/pages/BlogHome.jsx
+++ src/pages/BlogHome.jsx
@@ -14,13 +14,14 @@ const BlogHome = () => {
       <div className="bg-white">
         <div className="w-[70%] mx-auto">
           <Navbar />
-
+          {/* Hero headline */}
           <div className="flex flex-col w-[80%] mx-auto">
             <h1 className="mt-24 text-[60px] tracking-[-1.5px] leading-[60px] font-extrabold font-inter">
               Tutorials, Resources, Snippets for designers &
               React devs
             </h1>
 
+            {/* Sub-text */}
             <p className={`${styles.paragraph} mt-3 w-[80%]`}>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga perferendis, cumque officiis doloribus saepe vitae cum sint numquam ea laboriosam facilis.
             </p>
